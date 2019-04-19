feb9336cff5179fdd6c55b2a5214a10e6a400ceb
jinaroo <sohnjin@gmail.com>
2019-04-19 15:15:39 -0400

    added raycastbutton script

---
+++ CodeLab1-3DGame/Assets/Scripts/RaycastButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastButton : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // generate a "ray" variable
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        //visualize the raycast in the debug scene view
+        Debug.DrawRay(myRay.origin, myRay.direction * 100, Color.yellow);
+        
+        // initialize a "RaycastHit" variable
+        RaycastHit myRaycastHitInfo = new RaycastHit();
+        
+        // shoot raycast
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(myRay, out myRaycastHitInfo, 1000f))
+            {
+                Destroy(myRaycastHitInfo.collider.gameObject);
+                
+                // instead of destroying collider, change to a random sprite prefab from an array?
+            }
+        }
+    }
+}
